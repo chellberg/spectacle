1ceb74762f26c596be64b177ab1ce04bc7b24612
Corin Chellberg <corinchellberg@gmail.com>
2015-07-15 16:08:26 -0400

    add how to babel slide

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -1,7 +1,7 @@
 import React from "react/addons";
 
 import {
-  Appear, BlockQuote, Cite, CodePane, Deck, Fill,
+  Appear, BlockQuote, Cite, Code, CodePane, Deck, Fill,
   Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text
 } from "../src/spectacle";
 
@@ -92,7 +92,7 @@ export default class extends React.Component {
             </Text>
           </Appear>
         </Slide>
-        <Slide transition={['slide']} bgColor="black">
+        <Slide transition={['slide', 'spin']} bgColor="black">
           <Image src={images.possible.replace('/','')} margin="0px auto 40px" height="293px"/>
         </Slide>
         <Slide bgColor="primary">
@@ -231,6 +231,24 @@ export default class extends React.Component {
             source={require("raw!./inheritance.example")} />
         </Slide>
 
+        <Slide>
+          <Heading fit caps margin="-80px 0 40px 0">
+            How to Babel
+          </Heading>
+          <Code>
+            npm install babel
+          </Code>
+          <Text textSize={30} caps fill>
+            ...then...
+          </Text>
+          <Code>
+            babel index.js -o bundle.js
+          </Code>
+          <Text textSize={30} caps fill>
+            done!
+          </Text>
+        </Slide>
+
 
         <Slide>
           <Heading fill caps margin="-80px 0 40px 0">
@@ -251,66 +269,6 @@ export default class extends React.Component {
               https://hacks.mozilla.org/2015/04/es6-in-depth-an-introduction/
             </Text>
           </Link>
-        </Slide>
-
-        <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-          <Appear fid="1">
-            <Heading size={1} caps fit textColor="primary">
-              Full Width
-            </Heading>
-          </Appear>
-          <Appear fid="2">
-            <Heading size={1} caps fit textColor="tertiary">
-              Adjustable Darkness
-            </Heading>
-          </Appear>
-          <Appear fid="3">
-            <Heading size={1} caps fit textColor="primary">
-              Background Imagery
-            </Heading>
-          </Appear>
-        </Slide>
-
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
-          <Heading caps fit>Flexible Layouts</Heading>
-          <Layout>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Left
-              </Heading>
-            </Fill>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Right
-              </Heading>
-            </Fill>
-          </Layout>
-        </Slide>
-
-        <Slide transition={["slide"]} bgColor="black">
-          <BlockQuote>
-            <Quote>Wonderfully formatted quotes</Quote>
-            <Cite>Ken Wheeler</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={["slide", "spin"]} bgColor="primary">
-          <Heading caps fit size={1} textColor="tertiary">
-            Smooth
-          </Heading>
-          <Heading caps fit size={1} textColor="secondary">
-            Combinable Transitions
-          </Heading>
-        </Slide>
-
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <List>
-            <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
-            <ListItem><Appear fid="2">Autofit text</Appear></ListItem>
-            <ListItem><Appear fid="3">Flexbox layout system</Appear></ListItem>
-            <ListItem><Appear fid="4">React-Router navigation</Appear></ListItem>
-            <ListItem><Appear fid="5">PDF export</Appear></ListItem>
-            <ListItem><Appear fid="6">And...</Appear></ListItem>
-          </List>
         </Slide>
 
         <Slide transition={["spin", "slide"]} bgColor="tertiary">
