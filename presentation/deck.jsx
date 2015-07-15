2c5f463ad91bfc8bf0a7fb8e8fb63cff0f5d5f86
Corin Chellberg <corinchellberg@gmail.com>
2015-07-15 14:32:54 -0400

    got enough done that I'd be sad if I lost this so

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -12,7 +12,10 @@ import Interactive from "./interactive";
 const images = {
   city: require("./city.jpg"),
   kat: require("./kat.png"),
-  logo: require("./formidable-logo.svg")
+  logo: require("./formidable-logo.svg"),
+  possible: require("./possible.gif"),
+  babel: require("./babel.png"),
+  kangax: require("./kangax.png")
 };
 
 preloader([images.city, images.kat]);
@@ -21,26 +24,94 @@ export default class extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={800}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps textColor="black">
-            Spectacle
+        <Slide transition={["slide"]} bgColor="primary">
+          <Heading size={1} fill caps textColor="black">
+            ES6
           </Heading>
-          <Heading size={1} fit caps margin="-20px 0px">
-            A ReactJS Presentation Library
+          <Heading size={2} fit caps margin="">
+            Tomorrow's Javascript, today.
           </Heading>
-          <Heading size={2} fit caps textColor="black">
-            Where You Can Write Your Decks In JSX
-          </Heading>
-          <Link href="https://github.com/FormidableLabs/spectacle">
-            <Text bold caps textColor="tertiary">View on Github</Text>
-          </Link>
-          <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+          <Appear>
+            <Text fit textSize={1} caps textColor="tertiary">shamelessly stolen from babel</Text>
+          </Appear>
         </Slide>
-        <Slide transition={['slide']} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-          <Image src={images.kat.replace('/','')} margin="0px auto 40px" height="293px"/>
-          <Heading size={1} fit textColor="primary" textFont="secondary">
-            Wait what?
+        <Slide>
+          <Heading size={1} fit caps>
+            What is ES6?
           </Heading>
+          <Appear fid="1">
+            <Text textSize={60}>
+              ECMAScript 6 (aka ECMAScript 2015)
+            </Text>
+          </Appear>
+          <Appear fid="2">
+            <Text textSize={40}>
+              (formerly European Computer Manufacturers Association, now just nonsense letters)
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide>
+          <Heading size={1} caps fit>
+            warning: incoming generalization
+          </Heading>
+        </Slide>
+        <Slide>
+          <Heading fit caps>
+            ES5: Bad old JavaScript
+          </Heading>
+          <Appear fid="1">
+            <Heading fit caps margin="60px 0 0 0 ">
+              ES6: new good javascript
+            </Heading>
+          </Appear>
+          <Appear fid="2">
+            <Text textSize="60">
+              ...with a small catch
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide>
+          <Heading fit caps size={1} margin="-50px 0 20px 0">
+            Browser compatibility
+          </Heading>
+          <Appear>
+            <Link href="http://kangax.github.io/compat-table/es6/"><Image width="100%" src={images.kangax}/></Link>
+            <Text textSize="30">
+              http://kangax.github.io/compat-table/es6/
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide bgColor="black">
+          <Heading fit caps color="tertiary">
+            good news
+          </Heading>
+          <Appear>
+            <Text textSize="50" textColor="tertiary">
+              you can use ES6 today, painlessly
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide transition={['slide']} bgColor="black">
+          <Image src={images.possible.replace('/','')} margin="0px auto 40px" height="293px"/>
+        </Slide>
+        <Slide bgColor="primary">
+          <Heading size={2} fit caps margin="-40px auto auto auto">
+            ...thanks to Babel
+          </Heading>
+          <Image src={images.babel.replace('/', '')} margin="0px -50px 40px" height="593px"/>
+        </Slide>
+        <Slide>
+          <Heading size={1}>
+            okay. why should I?
+          </Heading>
+          <Appear>
+            <Text caps textSize={90} margin="40px 0 0 0">
+              good question
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide>
+
         </Slide>
         <Slide transition={['zoom', 'fade']} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
           <CodePane
@@ -48,6 +119,7 @@ export default class extends React.Component {
             source={require("raw!./deck.example")}
             margin="20px auto"/>
         </Slide>
+
         <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
           <Appear fid="1">
             <Heading size={1} caps fit textColor="primary">
@@ -65,6 +137,7 @@ export default class extends React.Component {
             </Heading>
           </Appear>
         </Slide>
+
         <Slide transition={["zoom", "fade"]} bgColor="primary">
           <Heading caps fit>Flexible Layouts</Heading>
           <Layout>
@@ -80,6 +153,7 @@ export default class extends React.Component {
             </Fill>
           </Layout>
         </Slide>
+
         <Slide transition={["slide"]} bgColor="black">
           <BlockQuote>
             <Quote>Wonderfully formatted quotes</Quote>
@@ -94,6 +168,7 @@ export default class extends React.Component {
             Combinable Transitions
           </Heading>
         </Slide>
+
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <List>
             <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
@@ -104,12 +179,7 @@ export default class extends React.Component {
             <ListItem><Appear fid="6">And...</Appear></ListItem>
           </List>
         </Slide>
-        <Slide transition={["slide"]} bgColor="primary">
-          <Heading size={1} caps fit textColor="tertiary">
-            Your presentations are interactive
-          </Heading>
-          <Interactive/>
-        </Slide>
+
         <Slide transition={["spin", "slide"]} bgColor="tertiary">
           <Heading size={1} caps fit textColor="primary">
             Made with love in Seattle by
